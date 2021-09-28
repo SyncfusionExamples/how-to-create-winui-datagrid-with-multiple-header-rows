0547d614893fa599ebc38deda8ecd056cb036f9c
MohanramAnbukkarasu <44049406+MohanramAnbukkarasu@users.noreply.github.com>
2021-09-28 18:21:51 +0530

    Add files via upload

---
+++ SfDataGrid_StackedHeaders_DataAnnotation/SfDataGridDemo/Model/Model.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SfDataGridDemo
+{
+    public partial class OrderInfo : INotifyPropertyChanged
+    {
+        private int _OrderID;
+
+        private string _CustomerID;
+
+        private double _unitPrice;
+
+        private int _Quantity;
+
+        private int _contactNumber;
+
+        private string _product;
+
+        private string _shipaddress;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OrderInfo"/> class.
+        /// </summary>
+        public OrderInfo()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets or sets the order ID.
+        /// </summary>
+        /// <value>The order ID.</value>
+        [Display(GroupName = "Order Details", Name = "Order ID")]
+        public int OrderID
+        {
+            get
+            {
+                return this._OrderID;
+            }
+            set
+            {
+                this._OrderID = value;
+                this.OnPropertyChanged("OrderID");
+            }
+        }
+
+       
+
+        /// <summary>
+        /// Gets or sets the Product.
+        /// </summary>
+        /// <value>The Product.</value>
+        [Display(GroupName = "Order Details", Name = "Product Name")]
+        public string ProductName
+        {
+            get
+            {
+                return this._product;
+            }
+            set
+            {
+                this._product = value;
+                this.OnPropertyChanged("ProductName");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets quantity
+        /// </summary>
+        /// <value>the quantity</value>     
+        [Display(GroupName = "Order Details", Name = "Quantity")]
+        public int Quantity
+        {
+            get
+            {
+                return this._Quantity;
+            }
+            set
+            {
+                _Quantity = value;
+                OnPropertyChanged("Quantity");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the unit price.
+        /// </summary>
+        /// <value>The unit price.</value>
+        [Display(GroupName = "Order Details", Name = "Unit Price")]
+        [DataType(DataType.Currency)]
+        public double UnitPrice
+        {
+            get
+            {
+                return _unitPrice;
+            }
+            set
+            {
+                _unitPrice = value;
+                OnPropertyChanged("UnitPrice");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the customer ID.
+        /// </summary>
+        /// <value>The customer ID.</value>
+        [Display(GroupName = "Customer Details", Name = "Customer ID")]
+        public string CustomerID
+        {
+            get
+            {
+                return this._CustomerID;
+            }
+            set
+            {
+                this._CustomerID = value;
+                this.OnPropertyChanged("CustomerID");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets quantity
+        /// </summary>
+        /// <value>the quantity</value>     
+        [Display(GroupName = "Customer Details", Name = "Contact Number")]
+        public int ContactNumber
+        {
+            get
+            {
+                return this._contactNumber;
+            }
+            set
+            {
+                _contactNumber = value;
+                OnPropertyChanged("ContactNumber");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the ShipAddress.
+        /// </summary>
+        /// <value>The ShipAddress.</value>
+        [Display(GroupName = "Shipment Details", Name = "ShipCountry")]
+        public string ShipCountry
+        {
+            get
+            {
+                return this._shipaddress;
+            }
+            set
+            {
+                this._shipaddress = value;
+                this.OnPropertyChanged("ShipCountry");
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
